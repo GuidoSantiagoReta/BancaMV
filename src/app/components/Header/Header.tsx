9dae6d55b654b3dba226337639c027dc941affb7
GuidoSantiagoReta <guidosantiagoreta@gmail.com>
2024-05-07 15:44:30 -0300

    centrando enlaces de navegación

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -23,7 +23,7 @@ export function Header(){
         : <HiOutlineMenuAlt2 className="block text-3xl md:hidden cursor-pointer text-secondary" onClick={() => setOpenMenu(!openMenu)} />
         }
           <div className={`flex flex-col items-center  ${openMenu ? 'block' : 'hidden'} w-full md:block md:w-auto`}>
-          <div className="flex flex-col p-4 mt-4 md:p-0 md:flex-row md:space-x-8 md:mt-0 md:border-0">
+          <div className="flex flex-col items-center justify-center p-4 mt-4 md:p-0 md:flex-row md:space-x-8 md:mt-0 md:border-0">
             {dataHeader.map(({id, name, idLink}) =>(
               <div key={id} className="px-3 transition-all duration-400 ease-in-out mb-4 py-4 md:mb-0">
                  <Link href={idLink} className="text-lg hover:text-secondary">{name}</Link>
